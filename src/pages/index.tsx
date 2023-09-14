62a3f44aed48613c8a726223018de08f17371c73
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-09-14 21:05:39 +0530

    Adding public images and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
-import Head from 'next/head';
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>Create Next App</title>
-			</Head>
 			<main>
 				<h1 className="text-sky-950">Hello world!!</h1>
 			</main>
