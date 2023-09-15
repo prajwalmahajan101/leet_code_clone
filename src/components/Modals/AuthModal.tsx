702ee240dc249adf15f8a7f028a091dbb0bd906f
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2023-09-15 12:33:38 +0530

    Added Form for Login, SignUp and Forgot Password

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from "react";
 import { IoClose } from "react-icons/io5";
+import Login from "@/components/Modals/Login";
+// import SignUp from "@/components/Modals/Signup";
+// import ForgotPassword from "@/components/Modals/ResetPassword";
 
 type AuthModalProps = {};
 
@@ -9,7 +12,7 @@ const AuthModal: FC<AuthModalProps> = ({}) => {
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"></div>
       <div className="w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
         <div className="relative w-full h-full mx-auto flex items-center justify-center">
-          <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
+          <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange-s to-slate-900 mx-6">
             <div className="flex justify-end p-2">
               <button
                 type="button"
@@ -19,6 +22,7 @@ const AuthModal: FC<AuthModalProps> = ({}) => {
                 <IoClose className="h-5 w-5" />
               </button>
             </div>
+            <Login />
           </div>
         </div>
       </div>
